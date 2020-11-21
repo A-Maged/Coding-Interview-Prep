9cd3696de6a97971c258ec1d2fdbe0ad004b4434
a-maged <maged.dev@gmail.com>
2020-11-21 21:06:25 +0200

    added comments and changed forEach to for

--- problems/02-smalles-subarray-with-sum.ts
+++ problems/02-smalles-subarray-with-sum.ts
@@ -2,23 +2,35 @@
   smallest subarray sum is >= to a given sum
 */
 
-console.log(run([4, 2, 2, 7, 1, 2, 1, 0], 8)); // 2
+console.log(run([4, 2, 2, 7, 8, 1, 2, 1, 0], 8)); // 2
 
 function run(arr: number[], targetSum: number) {
   let smallestSubarraySize = Infinity;
   let currentWindowSum = 0;
   let windowStart = 0;
 
-  arr.forEach((el, windowEnd) => {
-    currentWindowSum += el;
+  let count = 0;
 
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    /* increase window sum  as we progress*/
+    currentWindowSum += arr[windowEnd];
+
+    /* constraints are met? */
     while (currentWindowSum >= targetSum) {
+      /* calculate current window size */
       smallestSubarraySize = windowEnd + 1 - windowStart;
-      currentWindowSum -= arr[windowStart];
 
+      /* 
+        narrow the window from tail,
+        to reach smallest window possible while satisfying constraints 
+      */
+      currentWindowSum -= arr[windowStart];
       windowStart++;
     }
-  });
+    count++;
+  }
+
+  console.log(count);
 
   return smallestSubarraySize;
 }
