0e1885e8d75d1adc4d86628f5b8455bc6d239953
a-maged <maged.dev@gmail.com>
2020-11-22 23:09:59 +0200

    twoNumberSum again

--- problems/03-two-number-sum.py
+++ problems/03-two-number-sum.py
@@ -14,19 +14,38 @@
 
 
 # O(n) time | O(n) space
+# def twoNumberSum(array, targetSum):
+#     comps = {}
+
+#     for num in array:
+#         # complement(-1) = targetSum(10) - currentNum(11)
+#         complement = targetSum - num
+
+#         if complement in comps:
+#             return [complement, num]
+#         else:
+#             # complement(-1) = targetSum(10) - currentNum(11)
+#             # store "num", because next time we'll encounter the current "complement", but it will be treated as a currentNum
+#             comps[num] = True
+
+#     return []
+
+# O(nLog(n)) time | O(1) space
 def twoNumberSum(array, targetSum):
-    comps = {}
+    array.sort()
 
-    for num in array:
-        # complement(-1) = targetSum(10) - currentNum(11)
-        complement = targetSum - num
+    left = 0
+    right = len(array) - 1
 
-        if complement in comps:
-            return [complement, num]
+    while left < right:
+        comp = array[left] + array[right]
+
+        if comp == targetSum:
+            return [array[left], array[right]]
+        elif comp < targetSum:
+            left += 1
         else:
-            # complement(-1) = targetSum(10) - currentNum(11)
-            # store "num", because next time we'll encounter the current "complement", but it will be treated as a currentNum
-            comps[num] = True
+            right -= 1
 
     return []
 
